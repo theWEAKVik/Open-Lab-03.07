c695bb50625ff36b3fa6044d585b268cced647b5
Viktor <krpectv57@gmail.com>
2020-10-06 19:44:28 +0200

    <=

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            if ((people * each) <= total)
+                return true;
+            else
+                return false;
         }
     }
 }
